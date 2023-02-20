453b6e43a7927d528ae34e9e5b6797c25895ac15
Dmytro-1506 <kyunlik@gmail.com>
2023-02-20 16:23:58 +0100

    change to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,78 +1,74 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { ContactList } from './ContactList/ContactList';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 
-export class App extends Component {
-  state = {
-  contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ],
-  filter: '',
+export function App() {
+  const [filterName, setFilterName] = useState('');
+  const [contacts, setContacts] = useState(() => {
+    const localContacts = localStorage.getItem("contacts")
+    if (localContacts) {
+      return JSON.parse(localContacts)
+    } else {
+      return [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+    }
   }
+  );
   
-  addContact = (newContact) => {
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts))
+  }, [contacts])
+
+  const addContact = (newContact) => {
     const isExist = () => {
-      return this.state.contacts.find(contact => {
+      return contacts.find(contact => {
         return (contact.name === newContact.name)
       })
     }
     if (isExist()) {
       return alert(`${newContact.name} is already in contacts`)
     }
-    return this.setState(prevState => ({ contacts: [...prevState.contacts, newContact] }))
+    return setContacts([...contacts, newContact])
   }
 
-  filterContacts = (event) => {
-    this.setState({ filter: event.target.value })
+  const filterContacts = (event) => {
+    setFilterName(event.target.value)
   }
 
-  deleteContact = (event) => {
+  const deleteContact = (event) => {
     const contactToDel = event.currentTarget.name;
-    const stateContacts = this.state.contacts;
-    const newContactsList = stateContacts.filter(contact => {
+    const newContactsList = contacts.filter(contact => {
       if (contact.name === contactToDel) {
         return false
       } else {
         return contact
       }
     })
-    this.setState({ contacts: newContactsList })
+    setContacts(newContactsList)
   }
 
-  componentDidUpdate = () => {
-    localStorage.setItem("contacts", JSON.stringify(this.state.contacts))
-  }
-
-  componentDidMount = () => {
-    const localContacts = localStorage.getItem("contacts")
-    if (localContacts) {
-      this.setState({ contacts: JSON.parse(localContacts) })
-    }
-  }
-
-  render() {
-    return (
-      <div
-        style={{
-          width: '800px',
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          color: '#010101',
-          margin: '40px auto',
-        }}
-      >
-          <h1 className="title">Phonebook</h1>
-        <ContactForm addContact={this.addContact} />
-        <h2 className="title">Contacts</h2>
-        <Filter findContacts={this.filterContacts} />
-        <ContactList list={this.state.contacts} deleteContact={this.deleteContact} filter={this.state.filter} />
-      </div>
-    )
-  }
+  return (
+    <div
+      style={{
+        width: '800px',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        color: '#010101',
+        margin: '40px auto',
+      }}
+    >
+      <h1 className="title">Phonebook</h1>
+      <ContactForm addContact={addContact} />
+      <h2 className="title">Contacts</h2>
+      <Filter findContacts={filterContacts} />
+      <ContactList list={contacts} deleteContact={deleteContact} filterName={filterName} />
+    </div>
+  )
 }
--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,9 +1,9 @@
 import './ContactList.css'
 
-export const ContactList = ({ list, deleteContact, filter }) => {
+export const ContactList = ({ list, deleteContact, filterName }) => {
     const findedContacts = list.filter((element) => {
         return (element.name.toLowerCase().split(' ').find(name => {
-            return name.startsWith(filter)
+            return name.startsWith(filterName)
         })) && element
     })
 
